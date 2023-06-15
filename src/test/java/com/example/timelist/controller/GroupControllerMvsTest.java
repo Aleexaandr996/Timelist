838e617afa90f72812605566fd98d4fec5878d4d
Serhii Viunnikov <sv@estem.de>
2023-06-15 17:20:41 +0200

    TMLST-2: Test fix

--- src/test/java/com/example/timelist/controller/GroupControllerMvsTest.java
+++ src/test/java/com/example/timelist/controller/GroupControllerMvsTest.java
@@ -1,52 +1,54 @@
 package com.example.timelist.controller;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.example.timelist.beans.Group;
 import com.example.timelist.service.GroupService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(GroupController.class)
 class GroupControllerMvcTest {
-@MockBean
-    private GroupService groupService;
-    @Autowired
-    private GroupController groupController;
-    @Autowired
-    private MockMvc mockMvc;
 
-@Test
-    void createMockMvc(){
+  @MockBean
+  private GroupService groupService;
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void createMockMvc() {
     assertThat(mockMvc).isNotNull();
-    }
+  }
 
-    void WhenDoGetGroupsThenReturnAllGroups(){
+  @Test
+  void WhenDoGetGroupsThenReturnAllGroups() throws Exception {
 
-        Group group = new Group();
-        group.setName("MK-21");
-        String groupId = UUID.randomUUID().toString();
-        List<Group> expectedGroups = new ArrayList<>();
-        expectedGroups.add(group);
+    Group group = new Group();
+    group.setName("MK-21");
+    String groupId = UUID.randomUUID().toString();
+    List<Group> expectedGroups = new ArrayList<>();
+    expectedGroups.add(group);
 
-        when(groupService.getGroups()).thenReturn(expectedGroups);
+    when(groupService.getGroups()).thenReturn(expectedGroups);
 
-        mockMvc.perform(MockHttpServletRequestBuilder.get()).andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("{\"name\": \"MK-21\", \"groupId\": }"));
-    }
+    mockMvc.perform(MockMvcRequestBuilders.get("/groups")).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(content().json("{\n"
+            + "  \"name\": \"MK-21\",\n"
+            + "  \"groupId\": \"" + groupId + "\"\n"
+            + "}"));
+  }
 
 }
